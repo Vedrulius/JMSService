f84d1abe965b91f75d03a91c1e6099ebc88d7c9e
Mikhail Dolgov <otetc82@yandex.ru>
2021-04-06 12:10:40 +0300

    edit Person->add acksonXmlProperty

--- src/main/kotlin/com/mihey/jms/model/Person.kt
+++ src/main/kotlin/com/mihey/jms/model/Person.kt
@@ -1,5 +1,6 @@
 package com.mihey.jms.model
 
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty
 import javax.persistence.*
 
 @Entity
@@ -9,9 +10,12 @@ data class Person(
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
+    @JacksonXmlProperty(localName = "id")
     val id: Int?,
+    @JacksonXmlProperty(localName = "name")
     @Column(name = "name")
     val name: String,
     @Column(name = "last_name")
+    @JacksonXmlProperty(localName = "last-name")
     val lastName: String
 )
